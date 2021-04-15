2f4782733e8694ff725775e05860d507615675e9
Signior-X <b19188@students.iitmandi.ac.in>
2021-04-15 19:22:55 +0530

    Did some cleaning

---
+++ code.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class code {
+    public static void main(String args[]) {
+        System.out.println("code");
+
+    }
+}
---
+++ dsa/number-theory/README.md
@@ -0,0 +1,3 @@
+Some observations
+For very large numbers like 10**18 - 1 gives wrong sqrt
+we generally use sqrtl in cpp
